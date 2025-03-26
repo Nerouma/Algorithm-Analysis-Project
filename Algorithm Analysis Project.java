850ce40b985ca54b2fd71a1d19a96e61f8075266
Nerouma <164911930+Nerouma@users.noreply.github.com>
2025-03-26 18:43:08 -0400

    Update Algorithm Analysis Project.java

--- Algorithm Analysis Project.java
+++ Algorithm Analysis Project.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 import java.util.concurrent.RecursiveTask;
-import java.until.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinPool;
 
 class ParallelMergeSort extends RecursiveTask<int[]>{
   private int [] myArray;
@@ -11,12 +11,12 @@ class ParallelMergeSort extends RecursiveTask<int[]>{
 @Override
   protected int [] compute(){
     if(myArray.length <= 16) {
-      Arrays.sort(myArray)
+      Arrays.sort(myArray);
       return myArray;
     }
-    int mid = array.length/2;
-    int [] left = Arrays.copyOFRange(array, 0, mid);
-    int [] right = Arrays.copyOFRange(array, mid, array.length);
+    int mid = myArray.length/2;
+    int [] left = Arrays.copyOfRange(myArray, 0, mid);
+    int [] right = Arrays.copyOfRange(myArray, mid, myArray.length);
 
     ParallelMergeSort leftRun = new ParallelMergeSort(left);
     ParallelMergeSort rightRun = new ParallelMergeSort(right);
@@ -32,7 +32,7 @@ class ParallelMergeSort extends RecursiveTask<int[]>{
   {
     int [] merged = new int [left.length + right.length];
     int i = 0, j = 0, k = 0;
-    
+
 while (i < left.length && j < right.length) {
             merged[k++] = (left[i] < right[j]) ? left[i++] : right[j++];
         }
@@ -42,3 +42,17 @@ while (i < left.length && j < right.length) {
 
         return merged;
     }
+
+  public static void main(String [] args){
+    int [] myArray = {3, 6, 8, 10, 1, 2, 1};
+    long startTime = System.nanoTime();
+    ForkJoinPool pool = new ForkJoinPool(Runtime.getRuntime().availableProcessors());
+    int [] sortedArray = pool.invoke(new ParallelMergeSort(myArray));
+    long endTime = System.nanoTime();
+    long duration = ((endTime - startTime)/1000000);
+    System.out.println("Time taken: " + duration + " milliseconds");
+    System.out.println(Arrays.toString(sortedArray));
+
+    pool.close();
+  }
+}
